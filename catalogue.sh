9a7206a103507a36b4f0ab8f7ed5ce0c3bbac4b4
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-10-07 10:10:25 +0530

    adding catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -60,7 +60,7 @@ VALIDATE $? "enabling catalogue"
 systemctl start catalogue &>>LOGFILE
 VALIDATE $? "staring  catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /root/roboshop-project/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mango.repo"
 
 yum install mongodb-org-shell -y &>>LOGFILE
